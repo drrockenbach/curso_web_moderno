0c2f61128a345f3114415daee945f490fd2a5cb0
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2019-01-29 17:29:07 -0200

    Funções anônimas e callback aula 1

---
+++ exercicios-js/Funcao/callback1.js
@@ -0,0 +1,7 @@
+
+const fabricantes = ["Mercedes", "Audi", "BMW"]
+
+let imprimir = (nome, indice) => console.log(`${indice + 1}. ${nome} `)
+
+fabricantes.forEach(imprimir)
+fabricantes.forEach(fabricante => console.log(fabricante))
