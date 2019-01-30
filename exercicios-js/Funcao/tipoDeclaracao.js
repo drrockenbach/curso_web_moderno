4d6207664eafcab96a9194580a0900d8e83418b1
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2019-01-30 13:02:21 -0200

    Exemplos de callback e funções, funções contrutoras

---
+++ exercicios-js/Funcao/tipoDeclaracao.js
@@ -0,0 +1,29 @@
+
+// Para function declaration, pode executar ela antes, visto que o interpretador primeiro prepara essas funções antes de começar a executar.
+// As demais não são assim, elas primeiro precisam ser declaradas antes de executar.
+
+// Executando antes da declaração
+console.log(soma(3,4))
+
+// function declaration
+
+function soma(x, y) {
+    return x + y
+}
+
+
+// function expression
+
+const sub = function (x, y) {
+    return x - y
+}
+
+console.log(sub(3,4))
+
+// named function expression
+
+const mult = function mult(x, y) {
+    return x * y
+}
+
+console.log(mult(3,4))
