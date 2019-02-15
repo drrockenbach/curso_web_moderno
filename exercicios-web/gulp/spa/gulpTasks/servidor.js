7350e87ba7ea2b5b8ccd6470af8f6cb364b22cbd
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2019-02-15 12:00:36 -0200

    Projeto SPA: Adicionando webserver

--- exercicios-web/gulp/spa/gulpTasks/servidor.js
+++ exercicios-web/gulp/spa/gulpTasks/servidor.js
@@ -1,10 +1,21 @@
 
 const gulp = require('gulp')
+const watch = require('gulp-watch')
+const webServer = require('gulp-webserver')
 
 gulp.task('monitorarMudancas', () => {
-
+    watch('src/**/*.html' , () => gulp.start('app.html') ) // fica monitorando os htmls no src, quando tiver mudança, executa apenas o build dos htmls
+    watch('src/**/*.scss' , () => gulp.start('app.css') ) 
+    watch('src/**/*.js' , () => gulp.start('app.js') ) 
+    watch('src/assets/imgs/**/*.*' , () => gulp.start('app.imgs') ) 
+    // como dependências não são adicionadas com frequencia, não foi adicionado monitoramento
+    // daí para o gulp e roda de novo
 })
 
 gulp.task('servidor', ['monitorarMudancas'], () => {
-    
+    return gulp.src('build').pipe(webServer({
+        livereload: true,
+        port: 8081,
+        open: true
+    }))
 })
--- exercicios-web/gulp/spa/gulpfile.js
+++ exercicios-web/gulp/spa/gulpfile.js
@@ -10,12 +10,12 @@ require('./gulpTasks/servidor')
 gulp.task('default', () => {
     if (util.env.production) { // se for production, primeiro chama o deps e depois o app
                                // para passar a flag, rodar 'gulp --production'. No package.json tem um script para isso, com nome build entao 'npm build'
-        // sequence('deps', 'app')
-        gulp.start('deps', 'app')
+        sequence('deps', 'app')
+        // gulp.start('deps', 'app')
     } else {
-        // sequence('deps', 'app', 'servidor') // Sequence garante que será processado de forma serial, um atrás do outro.  
+        sequence('deps', 'app', 'servidor') // Sequence garante que será processado de forma serial, um atrás do outro.  
                                               // O gulp, em algumas versões, executa de forma paralela, então o servidor pode terminar antes de deps e app terminarem, 
                                               // o que geraria problemas
-        gulp.start('deps', 'app', 'servidor') // Se der problema assim, voltar para o sequence
+        // gulp.start('deps', 'app', 'servidor') // Se der problema assim, voltar para o sequence
     }
 })
