d347983d1e065baaf932f028d46ee1a841aede7c
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2019-02-15 14:37:46 -0200

    Webpack - carregando arquivo scss(sass)

--- exercicios-web/webpack/webpack.config.js
+++ exercicios-web/webpack/webpack.config.js
@@ -16,12 +16,13 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/, // se for arquivos com extensão .css
+                test: /\.s?[ac]ss$/, // primeiro "s" é opciona, depois pode ter "a" letra ou letra "c". Vai pegar ".css" ou ".scss" ou ".sass"
                 use: [
                     MiniCssExtractPlugin.loader, // Para extrair o css como arquivo e não jogar dentro do js como o style-loader faz
                     // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>, ou seja, não adiciona um arquivo .css na pasta de build. 
                                     // Ele pega todo o css e coloca no arquivo de saída, o principal.js
-                    'css-loader' // interpreta @import, url()...
+                    'css-loader', // interpreta @import, url()...
+                    'sass-loader'
                 ]
             }
         ]
