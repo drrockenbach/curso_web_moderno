5a010516ddd3a8f999cf3bd045bf404c9d2aa57b
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2019-02-15 11:36:33 -0200

    Projeto SPA: Build das dependências

--- exercicios-web/gulp/spa/gulpTasks/deps.js
+++ exercicios-web/gulp/spa/gulpTasks/deps.js
@@ -1,14 +1,23 @@
 // dependências
 
 const gulp = require('gulp')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 
 gulp.task('deps', ['deps.css', 'deps.fonts'])
 
+// O sequence, utilizado no gulpfile, só funciona se tiver o return nas tasks
 gulp.task('deps.css', () => {
-
+    return gulp.src([
+        'node_modules/font-awesome/css/font-awesome.css'
+    ])
+        .pipe(uglifycss({'uglifyComments': true}))
+        .pipe(concat('deps.min.css'))
+        .pipe(gulp.dest('build/assets/css'))
 })
 
 gulp.task('deps.fonts', () => {
-    
+    return gulp.src(['node_modules/font-awesome/fonts/*.*'])
+        .pipe(gulp.dest('build/assets/fonts'))
 })
 
