f19cf539008cbb93634f72a511cd3e9a359d4f53
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2019-02-15 14:26:23 -0200

    Webpack - externalizando arquivo css

--- exercicios-web/webpack/webpack.config.js
+++ exercicios-web/webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     mode:'development',
@@ -7,12 +8,18 @@ module.exports = {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        })
+    ],
     module: {
         rules: [
             {
                 test: /\.css$/, // se for arquivos com extensão .css
                 use: [
-                    'style-loader', // Adiciona CSS a DOM injetando a tag <style>, ou seja, não adiciona um arquivo .css na pasta de build. 
+                    MiniCssExtractPlugin.loader, // Para extrair o css como arquivo e não jogar dentro do js como o style-loader faz
+                    // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>, ou seja, não adiciona um arquivo .css na pasta de build. 
                                     // Ele pega todo o css e coloca no arquivo de saída, o principal.js
                     'css-loader' // interpreta @import, url()...
                 ]
