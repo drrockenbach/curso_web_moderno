e313b040de69a10b100be82553b2e8a7568f49cb
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2019-02-15 09:56:39 -0200

    Fundamentos Gulp - Exemplo com o fluxo de execução

--- exercicios-web/gulp/basico/gulpfile.js
+++ exercicios-web/gulp/basico/gulpfile.js
@@ -11,7 +11,12 @@ gulp.task('default', () => {
 
 gulp.task('copiar', ['antesTaskCopiar1', 'antesTaskCopiar2'], () => { // Array de tasks que devem ser executadas antes da task copiar 
                                                                       // e a função vai ser chamada quando a task copiar for invocada
-    console.log('copiar!!!!')
+    // gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt']) // Arquivos que vão ser utilizados
+    // gulp.src('pastaA/*.txt') // Não entra em subpastas
+    gulp.src('pastaA/**//*.txt') // Assim copia tanto o que está direto na pastaA, como as subpastas
+        // .pipe(transformacao1()) // padrão pipe and filter
+        // .pipe(transformacao2()) // Pipe line....
+        .pipe(gulp.dest('pastaB')) // copia os arquivos passados no src para a pastaB. Não remove da origem
 })
 
 gulp.task('antesTaskCopiar1', () => {
@@ -22,6 +27,16 @@ gulp.task('antesTaskCopiar2', () => {
     console.log('antesTaskCopiar2')
 })
 
-gulp.task('fim', () => {
-    console.log('fim')
+// gulp.task('fim', () => {
+//     console.log('fim')
+// })
+
+gulp.task('fim', ['fim1', 'fim2'] ) 
+
+gulp.task('fim1', () => {
+    console.log('fim1')
+})
+
+gulp.task('fim2', () => {
+    console.log('fim2')
 })
