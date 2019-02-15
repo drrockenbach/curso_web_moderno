cd720fd23fcedf30ccfaed9293b29059dd5fa539
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2019-02-15 14:09:16 -0200

    Webpack - adicionado arquivo css

--- exercicios-web/webpack/src/principal.js
+++ exercicios-web/webpack/src/principal.js
@@ -4,5 +4,7 @@ import Pessoa from './pessoa'
 
 import moduloA from './modulos/moduloA'
 
+import './assets/css/estilo.css'
+
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
--- exercicios-web/webpack/webpack.config.js
+++ exercicios-web/webpack/webpack.config.js
@@ -6,6 +6,18 @@ module.exports = {
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/, // se for arquivos com extensão .css
+                use: [
+                    'style-loader', // Adiciona CSS a DOM injetando a tag <style>, ou seja, não adiciona um arquivo .css na pasta de build. 
+                                    // Ele pega todo o css e coloca no arquivo de saída, o principal.js
+                    'css-loader' // interpreta @import, url()...
+                ]
+            }
+        ]
     }
 }
 
