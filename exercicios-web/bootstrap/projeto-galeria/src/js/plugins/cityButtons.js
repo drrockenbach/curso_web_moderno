0d96d68d8fd25d73fe1eeb0febac4a17ab1b8cfb
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2019-02-18 17:30:02 -0300

    Projeto galeria - Filtrar fotos por cidade. Ainda não está funcional, pois como está carregando via ajax, não está executando os scripts. Na próxima aula será demonstrado como ajustar isso

---
+++ exercicios-web/bootstrap/projeto-galeria/src/js/plugins/cityButtons.js
@@ -0,0 +1,51 @@
+
+import $ from "jQuery"
+
+const duration = 300
+
+function filterByCity(city) {
+    $('[wm-city]').each(function (i, e) {
+        const isTarget = $(this).attr('wm-city') == city || city === null
+
+        if (isTarget) {
+            $(this).parent().removeClass('d-none') // Remove a classe que esconde o pai, adicionado abaixo
+            $(this).fadeIn(duration) // Faz aparecer.
+        } else {
+            $(this).fadeOut(duration, () => {
+                $(this).parent().addClass('d-none') // para realmente esconder a imagem, se não fica bugado o layout
+            }) // Faz desaparecer.
+
+        }
+
+    })
+}
+
+
+$.fn.cityButtons = function() {
+    const cities = new Set // Set não permite repetição
+    $('[wm-city]').each(function(i, e) {
+        cities.add($(e).attr('wm-city')) // Pega o conteúdo de wm-city ex: New York
+    })
+
+    // Transforma cada cidade única em um botão
+    const btns = Array.from(cities).map(city => {
+        const btn = $('<button>').addClass(['btn', 'btn-info']).html(city)
+        btn.click(e => filterByCity(city))
+        return btn
+    })
+
+    //Cria botão todas
+    const btnAll = $('button').addClass(['btn', 'btn-info', 'active']).html('Todas')
+    btnAll.click(e => filterByCity(null))
+    btns.push(btnAll)
+
+    // Coloca todos os botões dentro de um grupo de botões
+    const btnGroup = $('<div>').addClass(['btn-group'])
+    btnGroup.append(btns)
+
+    // cityButtons.html(btnGroup)
+    $(this).html(btnGroup)
+    return this
+}
+
+$('[wm-city-buttons').cityButtons()
