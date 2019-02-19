57ce707623ecf2e5fef3a9baa14ce36c030d9d7e
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2019-02-19 10:28:51 -0300

    React - Componente de classe #02 - Input utilizando evento com arrow function ou passando o método com o bind do this

--- exercicios-web/react/exercicios/src/componentes/Saudacao.jsx
+++ exercicios-web/react/exercicios/src/componentes/Saudacao.jsx
@@ -6,6 +6,22 @@ export default class Saudacao extends Component {
         tipo: this.props.tipo,
         nome: this.props.nome
     }
+    
+    constructor(props) {
+        super(props) // Precisa chamar o super para que os properties sejam enviados para o super e preenchidos
+        
+        // Da pra inicializar o state aqui
+        // this.state = {
+        //     tipo: this.props.tipo,
+        //     nome: this.props.nome
+        // }
+        
+        // Faz bind do this do contexto atual, ou seja, do componente Saudacao para a function setTipo
+        // Sem isso, não da pra fazer a chamada nos eventos, onChange por exemplo, por que o this não apontaria para a classe atual
+        // Da pra fazer isso direto no evento
+        // this.setTipo = this.setTipo.bind(this)
+
+    }
 
     setTipo(e) {
         // console.log(e.target.value)
@@ -25,7 +41,12 @@ export default class Saudacao extends Component {
             <React.Fragment>
                 <h1>{tipo} {nome} !</h1>
                 <hr />
-                <input type="text" placeholder="Tipo..." value={tipo} onChange={e => this.setTipo(e)} />
+                {/* <input type="text" placeholder="Tipo..." value={tipo} onChange={e => this.setTipo(e)} /> */}
+
+                {/* Da pra utilizar assim, sem ser com arrow function, mas é necessário fazer bind do this, direto aqui ou conforme feito no construtor */}
+                <input type="text" placeholder="Tipo..." value={tipo} onChange={this.setTipo.bind(this)} />
+
+                {/* Preferir com arrow function */}
                 <input type="text" placeholder="Nome" value={nome} onChange={e => this.setNome(e)} />
             </React.Fragment>
         )
