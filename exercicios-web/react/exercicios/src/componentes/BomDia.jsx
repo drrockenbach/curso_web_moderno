b193e18b4af7834131e591a8b99a8084469444c7
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2019-02-19 09:45:03 -0300

    React - Componente com parâmetro

---
+++ exercicios-web/react/exercicios/src/componentes/BomDia.jsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+// import React from 'react';
+
+export default props => 
+    [
+        <h1 key="bomDia">Bom Dia {props.nome}! Idade: {props.idade}</h1>,
+        <h2 key="ateLogo">Até logo</h2>
+    ]
+
+export default props => 
+    <Fragment>
+        <h1>Bom Dia {props.nome}! Idade: {props.idade}</h1>
+        <h2>Até logo</h2>
+    </Fragment>    
+
+
+// Como última opção utilizar array para retornar mais de uma tag. preferir Fragment
+// export default props => 
+//     [
+//         <h1 key="bomDia">Bom Dia {props.nome}! Idade: {props.idade}</h1>,
+//         <h2 key="ateLogo">Até logo</h2>
+//     ]
--- exercicios-web/react/exercicios/src/index.js
+++ exercicios-web/react/exercicios/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Primeiro from './componentes/Primeiro'
+// import Primeiro from './componentes/Primeiro'
+import BomDia from './componentes/BomDia'
 
-ReactDOM.render(<Primeiro></Primeiro>, document.getElementById('root') )
+// ReactDOM.render(<Primeiro></Primeiro>, document.getElementById('root') )
+
+ReactDOM.render(<BomDia nome="Diomar" idade={10} />, document.getElementById('root') )
 
