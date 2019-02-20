db9aae868940c8793411557fc153c24b58626f65
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2019-02-20 09:31:11 -0300

    React - Projeto CRUD - Componente navegação

--- exercicios-web/react/crud/frontend/src/components/template/Nav.jsx
+++ exercicios-web/react/crud/frontend/src/components/template/Nav.jsx
@@ -1,7 +1,11 @@
 import './Nav.css'
 import React from 'react'
+import NavItem from './NavItem'
 
 export default props => 
 <aside className="menu-area">
-    Navegação
+    <nav className="menu">
+        <NavItem href="#/" icon="home" label="Início"/>
+        <NavItem href="#/users" icon="users" label="Usuários"/>
+    </nav>
 </aside>
---
+++ exercicios-web/react/crud/frontend/src/components/template/NavItem.jsx
@@ -0,0 +1,8 @@
+
+import React from 'react'
+
+export default props => 
+
+<a href={props.href}> 
+    <i className={`fa fa-${props.icon}`} ></i> {props.label}
+</a>
