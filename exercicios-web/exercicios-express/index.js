8007ec3dcd0b9b3e0e21673fbc091490a136c55b
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2019-02-21 14:45:09 -0300

    Express - Tratando a primeira requisição

--- exercicios-web/exercicios-express/index.js
+++ exercicios-web/exercicios-express/index.js
@@ -2,6 +2,12 @@ const express = require('express')
 
 const app = express()
 
+// Qualquer URL vai bater aqui
+// Use recebe qualquer tipo de requisição
+app.use((req, resp) => {
+    resp.send("iiiiiiiiiiii")
+})
+
 app.listen(3000, () => {
     console.log("Servidor iniciado...")
 })
