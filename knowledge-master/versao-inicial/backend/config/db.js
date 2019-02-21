dcb13dd9d8ca6bcb09a0d9d3930d204daf9e5468
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2019-02-21 17:16:08 -0300

    Projeto Final - Backend - Knex - criando e executando as migration, criação de tabelas. Diversos comentários de utilização no arquivo knexfile.js

--- knowledge-master/versao-inicial/backend/config/db.js
+++ knowledge-master/versao-inicial/backend/config/db.js
@@ -4,4 +4,9 @@ const config = require('../knexfile.js')
 // instancia knex com as configs
 const knex = require('knex')(config)
 
+
+// Executa as migrations ao iniciar o projeto. Caso as migrations já estejam criadas, não vai criar novamente. Isso é controlado pela tabela knex_migrations.
+// Evitar utilizar isso para produção
+// knex.migrate.latest([config])
+
 module.exports = knex
--- knowledge-master/versao-inicial/backend/knexfile.js
+++ knowledge-master/versao-inicial/backend/knexfile.js
@@ -7,11 +7,24 @@ Esse arquivo vai ser importado dentro de config/db.js
 
 ***************************************
 Comandos knex para criação das tabelas:
-Vai criar uma pasta migration com arquivos... Será explicado na próxima aula
+Vai criar uma pasta migration com arquivos onde serão criadas as tabelas do sistema
 
 knex migrate:make create_table_users
 knex migrate:make create_table_categories
 knex migrate:make create_table_articles
+
+- ****Criar o banco**** 
+- para rodar essas migrations utilizar o comando knex migrate:latest 
+- isso vai rodar todas até a última criada
+- No banco é criada uma tabela, knex_migrations, que controla as migrations que já foram executadas
+
+- *** Da pra executar isso pelo código, conforme exemplo no arquivo db.js. Porém, evitar isso para produção.
+
+- *** Rollback ****
+- para executar o rollback utilizar o comando "knex migrate:rollback"
+- Na tabela de controle, tem um campo chamado batch. Ali tem um id. A primeira vez que rodar o rollback, vai executar só os scripts da última criação.
+  Ex: Se tiver sido rodado primeiro o migration de users, esse vai ter o batch = 1. E depois rodar os demais, os outros vão ficar com batch = 2. 
+      Rodando o knex migrate:rollback, vai dar rollback apenas nos batch = 2. Para dar rollback no batch = 1, teria que rodar novamente
 
 */
 
