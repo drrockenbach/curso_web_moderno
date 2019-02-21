2ef64a45b24693056325558a44cf2b95b95d39ab
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2019-02-21 16:31:05 -0300

    Projeto Final - Backend - Knex - Criando arquivo de configuração de banco

---
+++ knowledge-master/versao-inicial/backend/knexfile.js
@@ -0,0 +1,50 @@
+// Update with your config settings.
+// ******************* Instruções *******************
+/*
+Para criar este arquivo, após ter instalado o knex global, com comando npm i -g knex.
+Rodar o comando knex init. Esse é o arquivo de configuração de conexão com o banco de dados (bancos relacionais)
+*/
+
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
