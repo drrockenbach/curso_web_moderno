b03f98d8d925ca8501df06447e28ad33d9c1c35d
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2019-02-26 14:45:21 -0300

    VueJS - Projeto Final - Frontend - Exibir Artigo

---
+++ knowledge-master/versao-inicial/frontend/src/components/article/ArticleById.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="article-by-id">
+        <PageTitle icon="fa fa-file-o" :main="article.name" :sub="article.description" />
+
+        <!--O conteúdo do artigo já vem formatado como HTML. É o que é cadastrado naquele componente completo no cadastro de artigos-->
+        <div class="article-content" v-html="article.content" /> 
+    </div>
+</template>
+
+<script>
+
+import { baseApiUrl } from '@/global'
+import axios from 'axios'
+import PageTitle from '../template/PageTitle'
+
+export default {
+    name: 'ArticleById',
+    components: { PageTitle },
+    data: function() {
+        return {
+            article: {}
+        }
+    },
+    mounted() {
+        const url = `${baseApiUrl}/articles/${this.$route.params.id}`
+        axios.get(url).then(res => this.article = res.data)
+    }
+
+}
+</script>
+
+<style>
+    .article-content {
+        background-color: #fff;
+        border-radius: 8px;
+        padding: 25px;
+    }
+
+    /* CSS para quando o conteúdo tiver formatação de código */
+    .article-content pre {
+        padding: 20px;
+        border-radius: 20px;
+        font-size: 1.2rem;
+        background-color: #1e1e1e;
+        color: #fff;
+    }
+
+    /* CSS para quando o conteúdo tiver imagem */
+    .article-content img {
+        max-width: 100%; /* Para limitar as imagens ao tamanho máximo do componente artigo, para que uma imagem muito grande não ultrapasse o componente */
+    }
+
+    .article-content :last-child {
+        margin-bottom: 0px;
+    }
+    
+</style>
